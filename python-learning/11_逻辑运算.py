907f7c89f805b0c052bc00d0d937564d28ad02d3
perfect11-1 <huyixiuger@163.com>
2025-09-17 22:37:11 +0800

    Create 11_逻辑运算.py
    python入门

---
+++ python-learning/11_逻辑运算.py
@@ -0,0 +1,12 @@
+distance = 2 #距离2公里
+time_abundant = 30 #时间充裕
+weather_if_good = True #天气是否好
+
+def how_to_go(distance, time_abundant, weather_if_good): # TODO: 为此函数编写单元测试
+    if distance < 3 and time_abundant > 20 and weather_if_good:
+        print('步行去')
+    else:
+        print('开车去')
+
+how_to_go(distance, time_abundant, weather_if_good)
+how_to_go(1, 20, True)
